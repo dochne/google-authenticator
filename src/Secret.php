af37b31a4ad685cde5b73a3732f32a9df9c19b68
Doug <dougnelson@silktide.com>
2016-09-19 11:01:52 -0500

    moved to using rawurlencode

--- src/Secret.php
+++ src/Secret.php
@@ -31,7 +31,7 @@ class Secret
      */
     public function getUri()
     {
-        return "otpauth://totp/".urlencode($this->getLabel())."?secret=".$this->getSecretKey()."&issuer=".urlencode($this->getIssuer());
+        return "otpauth://totp/".rawurlencode($this->getLabel())."?secret=".$this->getSecretKey()."&issuer=".rawurlencode($this->getIssuer());
     }
 
     /**
