fbb7fd739029a814729492c015bb276a17ce5cca
Cassandra Stone <cstone@dspolitical.com>
2021-05-24 09:30:36 -0400

    code review changes

--- src/QrImageGenerator/EndroidQrImageGenerator.php
+++ src/QrImageGenerator/EndroidQrImageGenerator.php
@@ -8,22 +8,24 @@ use Endroid\QrCode\QrCode;
 class EndroidQrImageGenerator implements QrImageGeneratorInterface
 {
     protected $size;
+    protected $writer;
 
-    public function __construct($size = 200)
+    public function __construct($size = 200, $writer = 'png')
     {
         if (!is_numeric($size)) {
             throw new \InvalidArgumentException("Size is required to be numeric");
         }
 
         $this->size = $size;
+        $this->writer = $writer;
     }
 
-    public function generateUri(Secret $secret, $fileType = 'png')
+    public function generateUri(Secret $secret)
     {
         $qrCode = new QrCode($secret->getUri());
         $qrCode->setSize($this->size);
 
-        $qrCode->setWriterByName($fileType);
+        $qrCode->setWriterByName($this->writer);
 
         return $qrCode->writeDataUri();
     }
